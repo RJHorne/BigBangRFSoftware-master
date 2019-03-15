8e6c734138ad136053081a6a198806f7dec24a6e
Robert James Horne <r.j.horne@kent.ac.uk>
2019-03-15 13:20:00 +0000

    revamp

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,7 +22,11 @@ namespace WpfApp2
         public bool hit = true;
         public bool yellow = false;
         public bool start = true;
+        int TxPowerInDbm = 30;
+        int greenV = -50;
+        int yellowV = -51;
         string ipAddress = "169.254.1.1";  //Default IP for the impinj reader 
+        String epcFilter = "";
 
         public MainWindow()
         {
@@ -33,12 +37,10 @@ namespace WpfApp2
 
         private void Initialise()
         {
-
             Settings settings = reader.QueryDefaultSettings();
             settings.Report.IncludeAntennaPortNumber = true;
             settings.Report.IncludePeakRssi = true;
-            // settings.Antennas.TxPowerMax = true;           
-            int TxPowerInDbm = boxValue();
+            // settings.Antennas.TxPowerMax = true;                       
             settings.Antennas.TxPowerInDbm = TxPowerInDbm;
             //settings.Antennas.RxSensitivityInDbm = 3;
             settings.Antennas.RxSensitivityMax = true;
@@ -51,45 +53,39 @@ namespace WpfApp2
 
         }
 
-        public int boxValue()
+        public void newSettings(string ip,int power,int green, int yellow, String epc)
         {
-            ListBoxItem lbi = lbxItems.SelectedItem as ListBoxItem;
-            String selItem = lbi.Content.ToString();
-            int dbValue = Convert.ToInt32(selItem);
-
-            return dbValue;
+            ipAddress = ip;
+            TxPowerInDbm = power;
+            greenV = green;
+            yellowV = yellow;
+            epcFilter = epc;
         }
 
         private void UpdateListbox(List<Tag> list)
         {
-            // Console.WriteLine(list.Count);
-
-            // Loop through each tag is the list and add it to the Listbox.
             foreach (var tag in list)
             {
                 // listTags.Items.Add(tag.Epc + ", " + tag.AntennaPortNumber + ", " + tag.PeakRssiInDbm + ", " + tag.Tid);
                 String EPCCap = tag.Epc.ToString();
-                //if (EPCCap.StartsWith("E280 1160") == true) // just looking for the start of the epc of a dogbone.
-                //{
+                if (EPCCap.StartsWith(epcFilter) == true) // just looking for the start of the epc of a dogbone.
+                {
                     epcBottom.Content = tag.Epc.ToString();
                     double rssi = tag.PeakRssiInDbm;
                     rssiLabel.Content = rssi.ToString();
-                    if (rssi < -15 && rssi > -50)
+                    if (rssi < -15 && rssi > greenV)
                     {
                         YellowLight.Fill = new SolidColorBrush(Colors.White);
                         GreenLight.Fill = new SolidColorBrush(Colors.Green);
                     }
-                    else if (rssi < -51 && rssi > -75)
+                    else if (rssi < yellowV && rssi > -75)
                     {
                         yellow = false;
                         YellowLight.Fill = new SolidColorBrush(Colors.Yellow);
                         GreenLight.Fill = new SolidColorBrush(Colors.White);
-
                     }
-
                     rssi = ((-rssi) - 15) * 3.8;
                     int rssiCompu = Convert.ToInt32(rssi);
-
                     r = 255 - rssiCompu;
                     g = 0 + rssiCompu;
                     CheckConstraints(r, 0, 255);
@@ -102,13 +98,11 @@ namespace WpfApp2
                     hit = true;
                     yesno.Content = "RSSI :\n" + tag.PeakRssiInDbm.ToString();
                     yesno.Background = new SolidColorBrush(Color.FromArgb(255, gb, rb, 0));
-
-                //}
-                //else
-                //{
-                //    epcLabel.Content = "Beware Foreign Tag Detected\nEPC: " + tag.Epc.ToString();
-
-                //}
+                }
+                else
+                {
+                    epcLabel.Content = "Beware Foreign Tag Detected\nEPC: " + tag.Epc.ToString();
+                }
             }
         }
 
@@ -170,11 +164,6 @@ namespace WpfApp2
 
         private void OnTagsReported(ImpinjReader sender, TagReport report)
         {
-            // This event handler gets called when a tag report is available.
-            // Since it is executed in a different thread, we cannot operate
-            // directly on UI elements (the Listbox) in this method.
-            // We must execute another method (UpdateListbox) on the main thread
-            // using BeginInvoke. We will pass updateListbox a List of tags.
             Action action = delegate ()
             {
 
@@ -183,17 +172,6 @@ namespace WpfApp2
             };
             Dispatcher.BeginInvoke(DispatcherPriority.Normal, action);
         }
-
-        //private void ButtonStop_Click(object sender, RoutedEventArgs e)
-        //{
-        //    // Don't call the Stop method if the
-        //    // reader is already stopped.
-        //    if (reader.QueryStatus().IsSingulating)
-        //    {
-        //        reader.Stop();
-        //    }
-        //}
-
 
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
@@ -275,13 +253,16 @@ namespace WpfApp2
             }
         }
 
-        private void epcPower_PreviewGiveFeedback(object sender, GiveFeedbackEventArgs e)
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
+            settings settingsWin = new settings();
+            settingsWin.ShowDialog();
+            epcFilter = settingsWin.ipAddressValue;
+            TxPowerInDbm = settingsWin.power;
+            greenV = settingsWin.greenValue;
+            yellowV = settingsWin.yellowValue;
+            epcFilter = settingsWin.epcFilter;
 
-        }
-
-        private void epcPower_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
 
         }
     }
--- settings.xaml.cs
+++ settings.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,6 +23,34 @@ namespace WpfApp2
         public settings()
         {
             InitializeComponent();
+           
+        }
+
+        private void DefaultsApply_Click(object sender, RoutedEventArgs e)
+        {
+            ipAddressTextBox.Text = "169.254.1.1";
+            powerBox.Text = "25";            
+            greenValueTextBox.Text = "-50";
+            yellowValueTextBox.Text = "-60";
+            epcFilterTextBox.Text = "E280 1160";
+        }
+
+
+       public int power { get; set; }
+       public string ipAddressValue { get; set; }
+       public int greenValue { get; set; }
+       public int yellowValue { get; set; }
+       public String epcFilter { get; set; }
+
+
+        private void SettingsApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ipAddressValue = ipAddressTextBox.Text;
+            power = Convert.ToInt32(powerBox.Text);
+            greenValue = Convert.ToInt32(greenValueTextBox.Text);
+            yellowValue = Convert.ToInt32(yellowValueTextBox.Text);
+            epcFilter = epcFilterTextBox.Text;
+            this.Close();
         }
     }
 }
